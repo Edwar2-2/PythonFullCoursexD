943c13dd085cd2a2a52d25ebd0f32443c9c998fa
unknown <onofreeduardos@gmail.com>
2020-05-22 01:17:40 -0500

    02Variables-tipos concatenacion

--- 02Variables-tipos/variables.py
+++ 02Variables-tipos/variables.py
@@ -7,3 +7,15 @@ texto = "Master en Python"
 texto2 = "Con ese man"
 print(texto)
 print(texto2)
+
+print("------------------------------------------")
+
+# Concatenación
+
+nombre = "Eduardo"
+apellidos = "Onofre"
+web = "EdOweb.mx"
+
+print(nombre + " " + apellidos + "-" + web)
+print(f"{nombre} {apellidos} - {web}") #INTERPOLACION la f me deja formatear dentro de un texto una variable
+print("Hola me llamo {} {} y mi web es: {}".format(nombre,apellidos,web))
