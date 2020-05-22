e42094cecf5faed4a3aec8c8f3697ae7eaf67f9d
unknown <onofreeduardos@gmail.com>
2020-05-22 01:09:14 -0500

    02Variables-tipos

--- 02Variables-tipos/tipos.py
+++ 02Variables-tipos/tipos.py
@@ -5,8 +5,28 @@ cadena = "Hola soy yo"
 entero = 9
 flotante  = 4.2
 booleano = True
+lista = [10,20,30,100,200]
+listaString = [10,"algo",34.5]
+tupla = ("master", "en", "Python")  #Es una lista de datos que no cambian o no pueden cambiar 
+diccionario = {
+
+    "nombre" : "Ese man",
+    "subnombre" : "nigd"
+
+
+} # Coleccion de datos con clave y valor
+
+rango = range(9) #imprime los valores de 0 - 9 su RANGO
+dato_byte = b"probando" # Si agrego la b directamente digo que es del tipo byte
+
+print(type(cadena))
 print(type(cadena)) # Imprime el tipo de dato
 print(type(entero))
 print(type(flotante))
 print(type(booleano))
+print(type(lista))
+print(type(listaString))
+print(type(diccionario))
+print(type(rango))
+print(type(dato_byte))
 
